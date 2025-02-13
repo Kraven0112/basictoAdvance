d5687003c2c887073327167231dbdd6da6fe788d
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-02-13 14:11:25 +0530

    add new one

--- ObjectCompare.java
+++ ObjectCompare.java
@@ -1,13 +1,15 @@
+// Comparing the numbers are same or not
+
 class Object1{
     int number1;
     Object1(int num1){
-        num1 = this.number1;
+        this.number1 = num1;
     }
 }
 class Object2{
     int number2;
     Object2(int num2){
-        num2 = this.number2;
+        this.number2 = num2;
     }
 }
 public class ObjectCompare {
@@ -15,10 +17,6 @@ public class ObjectCompare {
         Object1 obj1 = new Object1(25);
         Object2 obj2 = new Object2(25);
 
-        if(obj1.number1 == obj2.number2){
-            System.out.println("equal");
-        }else{
-            System.out.println("Not equal");
-        }
+        System.out.println(obj1.number1 == obj2.number2);
     }
 }
