8dee943e8b10714d9ebc8b34d77bf8b20f3e08c0
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-02-20 15:13:01 +0530

    add new files in a project

---
+++ ArraySum.java
@@ -0,0 +1,14 @@
+public class ArraySum {
+
+    public static void main(String a[]){
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+
+        int sum = 0;
+
+        for(int i = 0; i<array.length; i++){
+            sum = sum + array[i];
+        }
+
+        System.out.println("Total sum of array elements : "+sum);
+    }
+}
