dfb0902bdece03aecb92f960f6ad3c2698436773
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-02-23 00:48:48 +0530

    add new files

--- Literals.java
+++ Literals.java
@@ -2,7 +2,14 @@ public class Literals {
 
     public static void main(String args[]){
         // Literals in java programming
-        System.out.println(0b101);
-        System.out.println(0b1000);
+        System.out.println(0b101); //5 binary of 5 in java
+        System.out.println(0b1000);//8 binary of 8 in java
+        System.out.println(0xE); //hexadecimal of 14 in java
+        System.out.println(010); //8 octal of 8 in java
+        System.out.println(0b1000000000);
+
+        double num = 100_000;
+        System.out.println(num);
+        System.out.println(12703e100);
     }
 }
