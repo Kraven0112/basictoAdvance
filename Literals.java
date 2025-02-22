1ae9c90ab6cafccf06abf09369da12c9eb567786
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-02-23 00:20:55 +0530

    add new files

---
+++ Literals.java
@@ -0,0 +1,8 @@
+public class Literals {
+
+    public static void main(String args[]){
+        // Literals in java programming
+        System.out.println(0b101);
+        System.out.println(0b1000);
+    }
+}
