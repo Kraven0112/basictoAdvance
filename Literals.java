dde239574c4acfe44d640776c4072d2e3c868fc9
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-02-24 17:28:59 +0530

    importing function from another class

--- Literals.java
+++ Literals.java
@@ -1,6 +1,9 @@
 public class Literals {
 
     public static void main(String args[]){
+        Addition a = new Addition();
+       int total =  a.add(122,122,122);
+       System.out.println("Total "+ total);
         // Literals in java programming
         System.out.println(0b101); //5 binary of 5 in java
         System.out.println(0b1000);//8 binary of 8 in java
