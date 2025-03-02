bee31f27c5e166c8a79226cb8fae059ae028f84c
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-03-02 08:14:46 +0530

    new

---
+++ Enum.java
@@ -0,0 +1,21 @@
+enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+}
+
+public class Enum {
+
+    public static void main(String args[]) {
+        Day today = Day.SUNDAY;
+
+        String result = "";
+
+        result = switch (today) {
+            case SUNDAY, SATURDAY:
+                yield "week off";
+            default:
+                yield "working days";
+        };
+
+        System.out.println(result);
+    }
+}
