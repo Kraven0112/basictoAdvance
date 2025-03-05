7ec0404f7e010b9844908c4c4923f584f4068146
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-03-05 10:21:55 +0530

    add new

--- VarArgs.java
+++ VarArgs.java
@@ -8,11 +8,23 @@ public class VarArgs {
         return result;
     }
 
+    public String concatenate(String... words) {
+        String fullName = "";
+        for (String word : words) {
+            fullName += word;
+        }
+        return fullName;
+    }
+
     public static void main(String[] args) {
         VarArgs arg = new VarArgs();
 
         double totalSum = arg.addition(1, 2, 3, 4, 5, 6.6, 7.9, 8, 9.9, 10);
 
         System.out.println("Total sum is : " + totalSum);
+
+        String fullName = arg.concatenate("S", "us", "h", "il");
+
+        System.out.println(fullName);
     }
 }
